2eac1b4f0e720ad0ba9994c6e4afff8dd5dce5df
AnaFSaraiva <saraivanapaula2@gmail.com>
2023-06-20 22:07:49 -0300

    fix: simplifica o codigo

--- js/script.js
+++ js/script.js
@@ -1,20 +1,35 @@
 import colocaNumeros from "./numeros.js";
 
 const numeroIndisponivel = [96, 38, 23, 15, 119, 140]
+const inputNumero = document.querySelector('.input_numero')
+const btnCompra = document.querySelector('.btnCompra')
+
+/* ----- habilita/desabilita botão ----- */
+inputNumero.addEventListener('keyup', (event) => {
+
+  const value = parseInt(event.currentTarget.value);
+  const includes = numeroIndisponivel.includes(value)
+
+  if (includes) {
+    btnCompra.setAttribute("disabled", "")
+  } else {
+    btnCompra.removeAttribute("disabled", "")
+  }
+})
+/* --------------------------------------*/
 
 function marcaNumero() {
-  const btnCompra = document.querySelector('.btnCompra')
 
   btnCompra.onclick = () => {
 
     const numeroComprado = document.querySelector(".input_numero").value
 
     if (numeroComprado >= 1 && numeroComprado <= 170) {
-      document.querySelector('.erro').style.visibility = "hidden"
       numeroIndisponivel.push(parseInt(numeroComprado))
-      console.log(numeroIndisponivel)
+      document.querySelector('.erro').style.visibility = "hidden"
 
-      trocaCor(numeroComprado)
+      trocaCor()
+
     } else {
       document.querySelector('.erro').style.visibility = "unset"
     }
@@ -22,23 +37,25 @@ function marcaNumero() {
   };
 }
 
-function trocaCor(value) {
-  const numeroEscolhido = document.getElementById(`${value}`)
-  numeroEscolhido.classList.add("comprado");
-}
+function trocaCor() {
+  // const numeroEscolhido = document.getElementById(`${value}`)
+  // numeroEscolhido.classList.add("comprado");
 
-
-function numerosJaVendidos() {
   numeroIndisponivel.forEach(element => {
-    trocaCor(element)
+    const numeroEscolhido = document.getElementById(`${element}`)
+    numeroEscolhido.classList.add("comprado")
   });
 }
 
 
+// function numerosJaVendidos() {
+//   numeroIndisponivel.forEach(element => {
+//     trocaCor(element)
+//   });
+// }
+
 window.onload = () => {
   colocaNumeros()
   marcaNumero()
-  numerosJaVendidos()
-
+  trocaCor()
 }
-
