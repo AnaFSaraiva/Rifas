d4086a595d4d70a3ae6e06b3f29fa3bf87f60ed1
AnaFSaraiva <saraivanapaula2@gmail.com>
2023-07-11 23:38:17 -0300

    fix reset

--- js/numero-reset.js
+++ js/numero-reset.js
@@ -5,21 +5,22 @@ function resetaRifa() {
   const modal = document.querySelector('.modal')
   const btnConfirmar = document.querySelector('.btn_modal_confirmar')
   const btnCancelar = document.querySelector('.btn_modal_cancelar')
-  const numeroIndisponivel = pegarNumerosIndisponiveis()
-
+  
   btnReset.addEventListener("click", ()=> { modal.style.visibility = 'unset' });
   btnCancelar.addEventListener('click', () => {modal.style.visibility = 'hidden'});
   
   btnConfirmar.addEventListener('click', () => {
+    const numeroIndisponivel = pegarNumerosIndisponiveis()
+    
     modal.style.visibility = 'hidden';
     numeroIndisponivel.forEach(element => {
       const numeroResetado = document.getElementById(`${element}`)
       numeroResetado.classList.remove("comprado")
-  });
-  
-  numeroIndisponivel.splice(0, numeroIndisponivel.length)
-  localStorage.removeItem("compreiNumero")
-})
+    });
+    
+    localStorage.setItem("compreiNumero", JSON.stringify([]))
+
+  })
 
 }
 
